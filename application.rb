a333ca9ad6d3bbf773061a5ebd7faf2616ba9388
Rob Hitt <robhitt@gmail.com>
2016-12-14 09:46:38 -0500

    done

--- application.rb
+++ application.rb
@@ -1,10 +1,11 @@
 class Application
 
+
   def call(env)
+    name = "Robert Hitt"
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is #{name}!"
     resp.finish
   end
 
 end
-
